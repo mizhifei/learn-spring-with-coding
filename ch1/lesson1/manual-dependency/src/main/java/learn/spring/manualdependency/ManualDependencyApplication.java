a69b2cc13431866a55fcab9e781aa611d2056fb1
zhifeimi <278184634@qq.com>
2022-11-27 11:20:23 +0800

    Use spring-boot-starter-parent with dependencyManagement

--- ch1/lesson1/manual-dependency/src/main/java/learn/spring/manualdependency/ManualDependencyApplication.java
+++ ch1/lesson1/manual-dependency/src/main/java/learn/spring/manualdependency/ManualDependencyApplication.java
@@ -2,12 +2,19 @@ package learn.spring.manualdependency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ManualDependencyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ManualDependencyApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring!";
     }
 
 }

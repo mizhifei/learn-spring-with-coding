49a1bb64d2812da72728ea8c4087bef2b1cbc2a3
zhifeimi <278184634@qq.com>
2022-11-29 20:38:54 +0800

    1.add project hello-data-source 2.exposure actuator's web endpoints, because it is disabled by default.

---
+++ ch2/lesson1/hello-data-source/src/main/java/learn/spring/hellodatasource/HelloDataSourceApplication.java
@@ -0,0 +1,39 @@
+package learn.spring.hellodatasource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootApplication
+@Slf4j
+public class HelloDataSourceApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDataSourceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+    }
+
+    private void showConnection() throws SQLException {
+		log.info("--------------------------------------------------------------");
+        log.info(dataSource.toString());
+		log.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		Connection connection = dataSource.getConnection();
+        log.info("--------------------------------------------------------------");
+		log.info(connection.toString());
+        log.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		connection.close();
+	}
+}
